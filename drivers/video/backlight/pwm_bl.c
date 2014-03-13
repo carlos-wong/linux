0d80f7c8a7b75b39380b21b28f276e61ee696523
carlos <huaixian.huang@gmail.com>
2014-03-13 17:41:36 +0800

    sorry I am wrong. if not use this way, the backlight will zero after total flush the image

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -246,7 +246,7 @@ static int pwm_backlight_probe(struct platform_device *pdev)
 			 data->dft_brightness, data->max_brightness);
 		data->dft_brightness = data->max_brightness;
 	}
-	bl->props.brightness = 0;//data->dft_brightness;
+	bl->props.brightness = data->dft_brightness;
 	backlight_update_status(bl);
 	platform_set_drvdata(pdev, bl);
 	return 0;
